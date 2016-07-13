533abd9e173a2377b43a3b8fd0c3b506716de3ee
Den <Den@DESKTOP-9PL6KFE>
2016-07-13 22:11:05 +0300

    Add project files.

---
+++ finalCinema/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalCinema
+{
+    class Cinema
+    {
+        string isNull = null;
+        int summOfViewers = 0;
+       
+        Visitor[,] visitors = new Visitor[4, 5];
+        for (int i = 0; i < vi
+             
+    }
+}
---
+++ finalCinema/Visitor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalCinema
+{
+   public  class Visitor
+    {
+        private string _name;
+        private string _surname;
+        private bool _sex; // if true = Male, false = Female
+        private int _age;
+
+        Visitor()
+        {
+            int names = RandomProvider.GetThreadRandom().Next(5);
+            string[] name = new string[] { "Jack", "Asa", "Jayden", "Keiran", "Gans" };
+            _name = name[names];
+
+            int surnames = RandomProvider.GetThreadRandom().Next(6);
+            string[] surname = new string[] { "Black", "Akira", "James", "Lee", "Split", "May" };
+            _surname = surname[surnames];
+
+            _age = RandomProvider.GetThreadRandom().Next(90);
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public bool Sex
+        {
+
+            get { return _sex; }
+            set { _sex = value; }
+        }
+    }
+}
